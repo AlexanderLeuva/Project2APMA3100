15cee4f4c89c3ee8ef6ccfd3adabeaf895cc758a
Alexander Le <xnf8sy@virginia.edu>
2023-04-02 13:45:12 -0400

    Create methods that add to W, the total time. Imported random package to simulate events of P[Busy], P[Available], and P[Unavailable].

--- Simulate.py
+++ Simulate.py
@@ -1,4 +1,4 @@
-
+import random
 
 
 def simulate():
@@ -8,10 +8,47 @@ def simulate():
     '''
     # Variables/Flags
     unsuccessfulCounter = 0
-    successfulCounter = 0
+    successful = False
+    W = 0
 
     # Constants
     PROB_BUSY = 0.2
     PROB_AVAILABLE = 0.5
     PROB_UNAVAILABLE = 0.3
-    # TODO: Implement Simulation.
+    events  = ["Busy", "Available", "Unavailable"]
+    probabilities = [PROB_BUSY, PROB_AVAILABLE, PROB_UNAVAILABLE]
+
+    event = random.choices(events, probabilities)
+    print(event)
+
+    #while (successful != True) or (unsuccessfulCounter != 4):
+
+
+def Start():
+    '''
+    Takes 6 seconds to start
+    :return: 6
+    '''
+    return 6
+def Busy():
+    '''
+    Takes 3 seconds to return Busy Call
+    :return: 3
+    '''
+    return 3
+def Available():
+    print("Available")
+    # TODO Implement
+def Unavailable():
+    '''
+    Takes 25 additional seconds to wait for 5 rings and conclude that no one will answer
+    :return: 25
+    '''
+    return 25
+def End():
+    '''
+    Takes 1 second to end the call
+    :return: 1
+    '''
+    return 1
+
